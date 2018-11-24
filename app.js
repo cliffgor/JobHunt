dd5d0fe6a94241fef50951d2fcdd1d3effae3605
Cliff Gor <clierozigy@gmail.com>
2018-11-24 21:42:38 +0300

    added a search endpoint

--- app.js
+++ app.js
@@ -14,21 +14,26 @@ app.use(express.static(path.join(__dirname, 'assets')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
 
-app.get('/', function (req, res) {
-    let url = `https://indreed.herokuapp.com/api/jobs?q=web+developer&limit=50`;
-    axios({
-        method: 'get',
-        url
+
+app.get('/search', function (req, res) {
+    queries = req.query;
+    let url = `https://indreed.herokuapp.com/api/jobs`;
+    if (queries){
+    axios.get(url, {
+        params: queries
     })
-    .then(function (response) {
-        let jobs = response.data;
-        res.render("index", { title: "JobHunt", jobs: jobs});
+    .then(function(response){
+        res.render("search", { title: "JobHunt", jobs: response.data});
     })
-    .catch(function (error) {
+    .catch(function(error) {
         console.log(error);
     });
-
+    }
+    else {
+        res.render("search", {title: "JobHunt"})
+    }
 });
+
 
 app.listen(app.get('PORT'), function () {
     console.log('Express started on http://localhost:' +
