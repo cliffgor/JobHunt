6655edbc592a68d91efe2eddbf8050d5947b4abf
Cliff Gor <clierozigy@gmail.com>
2018-11-25 18:19:38 +0300

    modifications made to app.js file

--- app.js
+++ app.js
@@ -14,26 +14,28 @@ app.use(express.static(path.join(__dirname, 'assets')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
 
+app.get('/', function (req, res) {
+    res.render("index", {title:"Jobby"});
+});
 
 app.get('/search', function (req, res) {
     queries = req.query;
     let url = `https://indreed.herokuapp.com/api/jobs`;
     if (queries){
-    axios.get(url, {
+        axios.get(url, {
         params: queries
     })
     .then(function(response){
-        res.render("search", { title: "JobHunt", jobs: response.data});
+        res.render("search", { title: "Jobby", jobs: response.data});
     })
     .catch(function(error) {
         console.log(error);
     });
     }
     else {
-        res.render("search", {title: "JobHunt"})
+        res.render("search", {title: "Jobby"})
     }
 });
-
 
 app.listen(app.get('PORT'), function () {
     console.log('Express started on http://localhost:' +
